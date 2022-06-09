9fe6f21cd6614490a4de5e48a09bab3444cd9d1c
Dperedo <dperedo@gmail.com>
2022-06-09 15:48:54 -0400

    Se actualiza servicio

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -7,39 +7,55 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
-
   final String _baseUrl = 'http://localhost:8187';
-  //final String token = ''; 
+  //final String token = '';
   final storage = const FlutterSecureStorage();
   bool isLoading = true;
 
-
-  Future<String?> login( String email, String password ) async {
-
+  Future<String?> login(String email, String password) async {
     final Map<String, dynamic> authData = {
-      'email': email,
+      'username': email,
       'password': password,
-      'returnSecureToken': true
-    };print('1');print(readToken());
+      //'returnSecureToken': true
+    };
+    print('1');
+    print(readToken());
 
+/*
     final url = Uri.https(_baseUrl, '/api/v1/User/authenticate', {
       //'key': token
-      'Headers': 'Bearer ${ readToken() }'
-    });print('2');
+      //'Headers': 'Bearer ${ readToken() }'
+    });
+    */
 
-    final resp = await http.post(url, body: json.encode(authData));
-    final Map<String, dynamic> decodedResp = json.decode( resp.body );
-    print(decodedResp);
+    // final url =
+    //     Uri(host: 'localhost', port: 8187, path: '/api/v1/User/authenticate');
 
-    if ( decodedResp.containsKey('idToken') ) {
-        // Token hay que guardarlo en un lugar seguro
-        // decodedResp['idToken'];
-        await storage.write(key: 'token', value: decodedResp['idToken']);
-        return null;
+    final url = Uri.parse('http://localhost:8187/api/v1/User/authenticate');
+
+    print('2');
+
+    try {
+      final resp = await http.post(url, body: json.encode(authData), headers: {'Content-type': 'application/json'});
+      print('Después del post');
+      final Map<String, dynamic> decodedResp = json.decode(resp.body);
+
+      print(decodedResp);
+    } catch (e) {
+      print('ERROR!!!!!!');
+      print(e.toString());
+    }
+
+/*
+    if (decodedResp.containsKey('idToken')) {
+      // Token hay que guardarlo en un lugar seguro
+      // decodedResp['idToken'];
+      await storage.write(key: 'token', value: decodedResp['idToken']);
+      return null;
     } else {
       return decodedResp['error']['message'];
     }
-
+    */
   }
 
   Future logout() async {
@@ -48,10 +64,6 @@ class AuthService extends ChangeNotifier {
   }
 
   Future<String> readToken() async {
-
     return await storage.read(key: 'token') ?? '';
-
   }
-
-
 }
