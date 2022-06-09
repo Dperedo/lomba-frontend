b3ae991535908cab5d2d4c66771ed17bd77717ce
miguelemerson <mperedo@gmail.com>
2022-06-09 15:48:15 -0400

    cambios en authservice

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -7,39 +7,40 @@ import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService extends ChangeNotifier {
-
   final String _baseUrl = 'http://localhost:8187';
-  //final String token = ''; 
   final storage = const FlutterSecureStorage();
   bool isLoading = true;
 
-
-  Future<String?> login( String email, String password ) async {
-
+  Future<String?> login(String email, String password) async {
     final Map<String, dynamic> authData = {
-      'email': email,
-      'password': password,
-      'returnSecureToken': true
-    };print('1');print(readToken());
+      'username': email,
+      'password': password
+    };
+    print('1');
 
-    final url = Uri.https(_baseUrl, '/api/v1/User/authenticate', {
-      //'key': token
-      'Headers': 'Bearer ${ readToken() }'
-    });print('2');
+    final url = Uri.parse('http://localhost:8187/api/v1/User/authenticate');
 
-    final resp = await http.post(url, body: json.encode(authData));
-    final Map<String, dynamic> decodedResp = json.decode( resp.body );
-    print(decodedResp);
+    print('2');
 
-    if ( decodedResp.containsKey('idToken') ) {
-        // Token hay que guardarlo en un lugar seguro
-        // decodedResp['idToken'];
-        await storage.write(key: 'token', value: decodedResp['idToken']);
-        return null;
-    } else {
-      return decodedResp['error']['message'];
+    http.Response? resp;
+    Map<String, dynamic>? decodedResp;
+
+    try {
+      print(json.encode(authData));
+      resp = await http.post(url, body: json.encode(authData), headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Access-Control_Allow_Origin": "*"
+      });
+      decodedResp = json.decode(resp.body);
+      print(decodedResp);
+    } catch (e) {
+      print(e.toString());
     }
 
+    if (resp?.statusCode == 200) {}
+
+    print('3');
   }
 
   Future logout() async {
@@ -48,10 +49,6 @@ class AuthService extends ChangeNotifier {
   }
 
   Future<String> readToken() async {
-
     return await storage.read(key: 'token') ?? '';
-
   }
-
-
 }
