497a3668941a925d766add829f0dabddd18c44fb
miguelemerson <mperedo@gmail.com>
2023-03-01 11:37:43 -0300

    Update home_cubit_test.dart

--- test/presentation/home/bloc/home_cubit_test.dart
+++ test/presentation/home/bloc/home_cubit_test.dart
@@ -1,7 +1,6 @@
 import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:lomba_frontend/presentation/home/bloc/home_cubit.dart';
-import 'package:mockito/mockito.dart';
 
 main() {
   late HomeLiveState testStateChangeValue = HomeLiveState();
