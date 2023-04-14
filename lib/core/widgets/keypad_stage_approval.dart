7222e3c8d6c4e88d561efe9486d0d2c2cde78abf
miguelemerson <mperedo@gmail.com>
2023-04-14 16:18:04 -0400

    se estandarizan botones de aprobación y votos

--- lib/core/widgets/keypad_stage_approval.dart
+++ lib/core/widgets/keypad_stage_approval.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../domain/entities/workflow/post.dart';
+import '../../domain/entities/workflow/vote.dart';
 import '../../presentation/approved/bloc/approved_bloc.dart';
 import '../../presentation/approved/bloc/approved_cubit.dart';
 import '../../presentation/approved/bloc/approved_event.dart';
@@ -14,7 +15,11 @@ import '../../presentation/tobeapproved/bloc/tobeapproved_event.dart';
 
 class KeypadApprovalToBeApproved extends StatelessWidget {
   const KeypadApprovalToBeApproved(
-      {super.key, required this.context, required this.post, required this.statecubit, required this.keyValidate});
+      {super.key,
+      required this.context,
+      required this.post,
+      required this.statecubit,
+      required this.keyValidate});
   final BuildContext context;
   final Post post;
   final ToBeApprovedLiveState statecubit;
@@ -23,70 +28,17 @@ class KeypadApprovalToBeApproved extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        OutlinedButton(
-          style: ButtonStyle(
-            shape: MaterialStateProperty.resolveWith(
-              (states) => RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  color: Theme.of(context).secondaryHeaderColor,
-                  width: 2,
-                ),
-              ),
-            ),
-          ),
-          onPressed: post.votes
-                      .any((element) => element.value == -1) ||
-                  (statecubit.votes.containsKey(post.id) &&
-                      statecubit.votes[post.id] == -1)
-              ? null
-              : () {
-                  post.votes.clear();
-                  context
-                      .read<ToBeApprovedBloc>()
-                      .add(OnToBeApprovedVote(post.id, -1));
-                  context
-                      .read<ToBeApprovedLiveCubit>()
-                      .makeVote(post.id, -1);
-                },
-          child: const Icon(
-            Icons.close,
-            size: 35,
-            color: Colors.blue,)
-        ),
-        OutlinedButton(
-          style: ButtonStyle(
-            shape: MaterialStateProperty.resolveWith(
-              (states) => RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-                side: BorderSide(
-                  color: Theme.of(context).secondaryHeaderColor,
-                  width: 2,
-                ),
-              ),
-            ),
-          ),
-          onPressed: post.votes
-                      .any((element) => element.value == 1) ||
-                  (statecubit.votes.containsKey(post.id) &&
-                      statecubit.votes[post.id] == 1)
-              ? null
-              : () {
-                  post.votes.clear();
-                  context
-                      .read<ToBeApprovedBloc>()
-                      .add(OnToBeApprovedVote(post.id, 1));
-                  context
-                      .read<ToBeApprovedLiveCubit>()
-                      .makeVote(post.id, 1);
-                },
-          child: const Icon(
-            Icons.check,
-            size: 35,
-            color: Colors.blue,)
-        ),
+        showRejectButton(context, post.id, post.votes, statecubit.votes, () {
+          post.votes.clear();
+          context.read<ToBeApprovedBloc>().add(OnToBeApprovedVote(post.id, -1));
+          context.read<ToBeApprovedLiveCubit>().makeVote(post.id, -1);
+        }),
+        showApproveButton(context, post.id, post.votes, statecubit.votes, () {
+          post.votes.clear();
+          context.read<ToBeApprovedBloc>().add(OnToBeApprovedVote(post.id, 1));
+          context.read<ToBeApprovedLiveCubit>().makeVote(post.id, 1);
+        }),
       ],
     );
   }
@@ -94,7 +46,11 @@ class KeypadApprovalToBeApproved extends StatelessWidget {
 
 class KeypadApprovalApproved extends StatelessWidget {
   const KeypadApprovalApproved(
-      {super.key, required this.context, required this.post, required this.statecubit, required this.keyValidate});
+      {super.key,
+      required this.context,
+      required this.post,
+      required this.statecubit,
+      required this.keyValidate});
   final BuildContext context;
   final Post post;
   final ApprovedLiveState statecubit;
@@ -104,69 +60,19 @@ class KeypadApprovalApproved extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        OutlinedButton(
-          style: ButtonStyle(
-            shape: MaterialStateProperty.resolveWith(
-              (states) => RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  color: Theme.of(context).secondaryHeaderColor,
-                  width: 2,
-                ),
-              ),
-            ),
-          ),
-          onPressed: post.votes
-                      .any((element) => element.value == -1) ||
-                  (statecubit.votes.containsKey(post.id) &&
-                      statecubit.votes[post.id] == -1)
-              ? null
-              : () {
-                  post.votes.clear();
-                  context
-                      .read<ApprovedBloc>()
-                      .add(OnApprovedVote(post.id, -1));
-                  context
-                      .read<ApprovedLiveCubit>()
-                      .makeVote(post.id, -1);
-                },
-          child: const Icon(
-            Icons.close,
-            size: 35,
-            color: Colors.blue,)
+        showRejectButton(context, post.id, post.votes, statecubit.votes, () {
+          post.votes.clear();
+          context.read<ApprovedBloc>().add(OnApprovedVote(post.id, -1));
+          context.read<ApprovedLiveCubit>().makeVote(post.id, -1);
+        }),
+        const SizedBox(
+          width: 5,
         ),
-        const SizedBox(width: 5,),
-        OutlinedButton(
-          style: ButtonStyle(
-            shape: MaterialStateProperty.resolveWith(
-              (states) => RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  color: Theme.of(context).secondaryHeaderColor,
-                  width: 2,
-                ),
-              ),
-            ),
-          ),
-          onPressed: post.votes
-                      .any((element) => element.value == 1) ||
-                  (statecubit.votes.containsKey(post.id) &&
-                      statecubit.votes[post.id] == 1)
-              ? null
-              : () {
-                  post.votes.clear();
-                  context
-                      .read<ApprovedBloc>()
-                      .add(OnApprovedVote(post.id, 1));
-                  context
-                      .read<ApprovedLiveCubit>()
-                      .makeVote(post.id, 1);
-                },
-          child: const Icon(
-            Icons.check,
-            size: 35,
-            color: Colors.blue,)
-        ),
+        showApproveButton(context, post.id, post.votes, statecubit.votes, () {
+          post.votes.clear();
+          context.read<ApprovedBloc>().add(OnApprovedVote(post.id, 1));
+          context.read<ApprovedLiveCubit>().makeVote(post.id, 1);
+        }),
       ],
     );
   }
@@ -174,7 +80,11 @@ class KeypadApprovalApproved extends StatelessWidget {
 
 class KeypadApprovalRejected extends StatelessWidget {
   const KeypadApprovalRejected(
-      {super.key, required this.context, required this.post, required this.statecubit, required this.keyValidate});
+      {super.key,
+      required this.context,
+      required this.post,
+      required this.statecubit,
+      required this.keyValidate});
   final BuildContext context;
   final Post post;
   final RejectedLiveState statecubit;
@@ -184,70 +94,94 @@ class KeypadApprovalRejected extends StatelessWidget {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        OutlinedButton(
-          style: ButtonStyle(
-            shape: MaterialStateProperty.resolveWith(
-              (states) => RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  color: Theme.of(context).secondaryHeaderColor,
-                  width: 2,
-                ),
-              ),
-            ),
-          ),
-          onPressed: post.votes
-                      .any((element) => element.value == -1) ||
-                  (statecubit.votes.containsKey(post.id) &&
-                      statecubit.votes[post.id] == -1)
-              ? null
-              : () {
-                  post.votes.clear();
-                  context
-                      .read<RejectedBloc>()
-                      .add(OnRejectedVote(post.id, -1));
-                  context
-                      .read<RejectedLiveCubit>()
-                      .makeVote(post.id, -1);
-                },
-          child: const Icon(
-            Icons.close,
-            size: 35,
-            color: Colors.blue,)
+        showRejectButton(context, post.id, post.votes, statecubit.votes, () {
+          post.votes.clear();
+          context.read<RejectedBloc>().add(OnRejectedVote(post.id, -1));
+          context.read<RejectedLiveCubit>().makeVote(post.id, -1);
+        }),
+        const SizedBox(
+          width: 5,
         ),
-        const SizedBox(width: 5,),
-        OutlinedButton(
-          style: ButtonStyle(
-            shape: MaterialStateProperty.resolveWith(
-              (states) => RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-                side: BorderSide(
-                  color: Theme.of(context).secondaryHeaderColor,
-                  width: 2,
-                ),
-              ),
-            ),
-          ),
-          onPressed: post.votes
-                      .any((element) => element.value == 1) ||
-                  (statecubit.votes.containsKey(post.id) &&
-                      statecubit.votes[post.id] == 1)
-              ? null
-              : () {
-                  post.votes.clear();
-                  context
-                      .read<RejectedBloc>()
-                      .add(OnRejectedVote(post.id, 1));
-                  context
-                      .read<RejectedLiveCubit>()
-                      .makeVote(post.id, 1);
-                },
-          child: const Icon(
-            Icons.check,
-            size: 35,
-            color: Colors.blue,)
-        ),
+        showApproveButton(context, post.id, post.votes, statecubit.votes, () {
+          post.votes.clear();
+          context.read<RejectedBloc>().add(OnRejectedVote(post.id, 1));
+          context.read<RejectedLiveCubit>().makeVote(post.id, 1);
+        }),
       ],
     );
   }
+}
+
+OutlinedButton showApproveButton(
+    BuildContext context,
+    String postId,
+    List<Vote> postVotes,
+    Map<String, int> cubitVotes,
+    VoidCallback onPressedFunction) {
+  return OutlinedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith(
+          (states) => isVoted(1, postId, postVotes, cubitVotes)
+              ? Colors.yellow
+              : Colors.white,
+        ),
+        shape: MaterialStateProperty.resolveWith(
+          (states) => RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            side: BorderSide(
+              color: Theme.of(context).secondaryHeaderColor,
+              width: 2,
+            ),
+          ),
+        ),
+      ),
+      onPressed:
+          isVoted(1, postId, postVotes, cubitVotes) ? null : onPressedFunction,
+      child: Icon(
+        Icons.check,
+        size: 35,
+        color: isVoted(1, postId, postVotes, cubitVotes)
+            ? Colors.black
+            : Colors.blue,
+      ));
+}
+
+OutlinedButton showRejectButton(
+    BuildContext context,
+    String postId,
+    List<Vote> postVotes,
+    Map<String, int> cubitVotes,
+    VoidCallback onPressedFunction) {
+  return OutlinedButton(
+      style: ButtonStyle(
+        backgroundColor: MaterialStateProperty.resolveWith(
+          (states) => isVoted(-1, postId, postVotes, cubitVotes)
+              ? Colors.yellow
+              : Colors.white,
+        ),
+        shape: MaterialStateProperty.resolveWith(
+          (states) => RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+            side: BorderSide(
+              color: Theme.of(context).secondaryHeaderColor,
+              width: 2,
+            ),
+          ),
+        ),
+      ),
+      onPressed:
+          isVoted(-1, postId, postVotes, cubitVotes) ? null : onPressedFunction,
+      child: Icon(
+        Icons.close,
+        size: 35,
+        color: isVoted(-1, postId, postVotes, cubitVotes)
+            ? Colors.black
+            : Colors.blue,
+      ));
+}
+
+bool isVoted(int voteValue, String postId, List<Vote> postVotes,
+    Map<String, int> cubitVotes) {
+  return postVotes.any((element) => element.value == voteValue) ||
+      (cubitVotes.containsKey(postId) && cubitVotes[postId] == voteValue);
 }
