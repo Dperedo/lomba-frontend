9a8a737ea5a037bf3f89f57d0cab9b8133b8001b
miguelemerson <mperedo@gmail.com>
2023-05-09 10:12:18 -0400

    se incluye página de diseño

---
+++ lib/design.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:lomba_frontend/presentation/design/page/design_page.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //usePathUrlStrategy();
+
+  runApp(const MyDesign());
+}
+
+class MyDesign extends StatefulWidget {
+  const MyDesign({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _MyDesign();
+}
+
+class _MyDesign extends State<MyDesign> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'App Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.cyan,
+        ),
+        home: const DesignPage());
+  }
+}
