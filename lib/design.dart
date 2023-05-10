9ae98ab00d044a58b701d4da9dfd39666000fe80
miguelemerson <mperedo@gmail.com>
2023-05-10 17:48:04 -0400

    Update design.dart

--- lib/design.dart
+++ lib/design.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:lomba_frontend/presentation/design/page/design_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //usePathUrlStrategy();
-
+  //debugPaintSizeEnabled = true;
   runApp(const MyDesign());
 }
 
